c7099612a90db8eee70349539101a2f137f5b677
Dongare, Rahul <Rahul.Dongare@cdk.com>
2021-01-05 16:01:51 +0530

    Added First Cut of Code

---
+++ GildedRose-Refactoring-Kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThat;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+    @Test
+    @DisplayName("At the end y, our ssystem lowers both values for every item") {
+        MatchAssert.assertThat();
+    }
+
+}
